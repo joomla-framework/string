f9fe41afffd28535b29b2875d48eaaf56532412a
Joomla Jenkins <release+joomla-jenkins@joomla.org>
2020-06-05 06:04:28 -0500

    Tagging release 2.0.0-beta

--- src/Inflector.php
+++ src/Inflector.php
@@ -2,7 +2,7 @@
 /**
  * Part of the Joomla Framework String Package
  *
- * @copyright  Copyright (C) 2005 - 2019 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
@@ -32,7 +32,7 @@ class Inflector extends DoctrineInflector
 	 * The inflector rules for countability.
 	 *
 	 * @var    array
-	 * @since  __DEPLOY_VERSION__
+	 * @since  2.0.0-beta
 	 */
 	private static $countable = [
 		'rules' => [
